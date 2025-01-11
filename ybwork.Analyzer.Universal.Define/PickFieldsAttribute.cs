698690a3d1f3eae2ee3d38423de3a607b3623cc5
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2025-01-11 14:51:27 +0800

    增加PickFieldsAttribute源生成器，用于从从其他类型中挑选字段

---
+++ ybwork.Analyzer.Universal.Define/PickFieldsAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ybwork.Analyzer.Universal
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class PickFieldsAttribute : Attribute
+    {
+        public readonly Type PickTpe;
+        public PickFieldsAttribute(Type type)
+        {
+            PickTpe = type;
+        }
+    }
+}
